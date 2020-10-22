6de73d0847361377db07873b4cba03f9b2906108
itamar124812 <itamar124812@gmail.com>
2020-10-22 11:53:58 +0300

    Initial code

---
+++ dotNET5781_00_0955_1050/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNET5781_00_0955_1050
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
