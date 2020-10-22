23db3232ce0740b5d0a01430e98904ac97015274
itamar124812 <itamar124812@gmail.com>
2020-10-22 12:05:02 +0300

    initial code+welcome4072

---
+++ dotNET5781_00_0955_1050/Program4072.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNET5781_00_0955_1050
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome_4072();
+            welcome_5426();
+            Console.ReadKey();
+        }
+        static partial void welcome_5426();
+        private static void welcome_4072()
+        {
+            Console.WriteLine("enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
