8e79cf5ed72a6db4f963bb50890daaf08fcb0f4a
ariel benshushan <benshoshan60@gmail.com>
2020-10-25 16:45:47 +0200

    hello

---
+++ dotNET5781_00_0955_1050/Program5426.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNET5781_00_0955_1050
+{
+    partial class Program
+    {
+        static partial void welcome_5426()
+        {
+            Console.WriteLine("I am here");
+        }
+    }
+}
