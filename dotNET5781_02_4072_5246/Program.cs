0bae38c5395ea2a54d5f834563e94e2121280e19
itamar124812 <itamar124812@gmail.com>
2020-11-03 10:05:36 +0200

    I started exe2

---
+++ dotNET5781_02_4072_5246/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNET5781_02_4072_5246
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
