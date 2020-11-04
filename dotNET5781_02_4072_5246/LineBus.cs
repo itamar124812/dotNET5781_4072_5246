d05dd7cd662af9e5e48cd3907cafd02b328664af
itamar124812 <itamar124812@gmail.com>
2020-11-04 18:07:52 +0200

    I made a few changes in the program.

--- dotNET5781_02_4072_5246/LineBus.cs
+++ dotNET5781_02_4072_5246/LineBus.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Services;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,9 +12,23 @@ namespace dotNET5781_02_4072_5246
     {
         List<BusLineStation> The_line_bus = new List<BusLineStation>();
        private  BusLineStation final_stop;
-        public BusLineStation Final_stop { set{ if (value == The_line_bus.Last()) final_stop = value; }get { return final_stop; } }
+        public BusLineStation Final_stop { set{ if (value == The_line_bus.Last()) final_stop = value; else final_stop = The_line_bus.Last(); } get { return final_stop; } }
         private BusLineStation start_station;
-        private int bus_line_key;
-        private int Area;
+        public int bus_line_key;
+        private area Area;
+        public int  AREA { set{ if (value < 0 || value > 4) throw new ArgumentException("There are only five areas. The first starts at 0 and the last ends at 4"); else Area = (area)value; } get { return (int)Area; } }
+        public override string ToString()
+        {
+            string result = string.Format("Bus line key:{0} ,The area: {1} ,Station numbers:", bus_line_key,Area);
+            foreach(BusLineStation A in The_line_bus)
+            {
+                result += A.BusStopkey + " ";
+            }
+            return result;
+        }
+        public void enter_a_new_stop()
+        {
+
+        }
     }
 }
--- dotNET5781_02_4072_5246/Program.cs
+++ dotNET5781_02_4072_5246/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,30 @@ namespace dotNET5781_02_4072_5246
     {
         static void Main(string[] args)
         {
+            int choice=0;
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("enter 1 for addition, 2 for Deletion, 3 for search, 4 for exit:");
+                string input = Console.ReadLine();
+                int.TryParse(input, out choice);
+                switch(choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("This number is invalid.");
+                        break;
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
