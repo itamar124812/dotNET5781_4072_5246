ca86cb662a1b55d3198fa706b197894ea5040002
itamar124812 <itamar124812@gmail.com>
2020-11-21 22:20:20 +0200

    open exe3A.

---
+++ dotNET5781_3A_4072_5246/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace dotNET5781_3A_4072_5246
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
