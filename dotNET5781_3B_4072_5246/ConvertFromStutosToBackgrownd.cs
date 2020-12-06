06274ad94472cea5497eccefde00bd9faa10581e
itamar124812 <itamar124812@gmail.com>
2020-12-06 03:02:26 +0200

    "Ex 3B Final commit"

--- dotNET5781_3B_4072_5246/ConvertFromStutosToBackgrownd.cs
+++ dotNET5781_3B_4072_5246/ConvertFromStutosToBackgrownd.cs
@@ -11,7 +11,7 @@ using System.Windows.Media;
 
 namespace dotNET5781_3B_4072_5246
 {
-    class ConvertFromStutosToBackgrownd : IValueConverter
+    class ConvertFromStutosToBackgrownd : IValueConverter 
     {
        public object Convert(object convertint,Type targetType,    object parameter,  CultureInfo culture)
         {
