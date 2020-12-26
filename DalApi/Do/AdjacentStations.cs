53e60a5d41be0dbe414838d8bbe363af0119e499
itamar124812 <itamar124812@gmail.com>
2020-12-26 22:43:33 +0200

    Add Windows

--- DalApi/Do/AdjacentStations.cs
+++ DalApi/Do/AdjacentStations.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace DO
+namespace DalApi.DO
 {
     class AdjacentStations
     {
--- DalApi/Do/BusStations.cs
+++ DalApi/Do/BusStations.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DO
+namespace DalApi.DO
 {
     class Bus_stations
     {
