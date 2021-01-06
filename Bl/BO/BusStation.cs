ee0c0c21510050ce8d2b23763d9fe4573e651bd1
ariel benshushan <benshoshan60@gmail.com>
2021-01-06 22:10:25 +0200

    I did my part

---
+++ Bl/BO/BusStation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bl.BO
+{
+    class BusStation
+    {
+       List<StationLine> LinesPassingThrough;
+        public int StationNumber { get; set }
+
+    }
+}
---
+++ Bl/BO/StationLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bl.BO
+{
+    class StationLine
+    {
+        public int IDLine { get; set; }
+        public int LineNumber { get; set; }
+        public String LastStop;
+        public TimeSpan Arrivaltimes { get; set; }
+
+
+
+    }
+}
